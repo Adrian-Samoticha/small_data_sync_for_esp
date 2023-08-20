ceddf5fe334939925ec74da8690f544ed6c3e245
Adrian Samoticha <adrian@samoticha.de>
2023-08-20 11:11:06 +0200

    add test for `equals`

--- examples/SmallDataSyncExample/test/test_data_object/test_data_object.cpp
+++ examples/SmallDataSyncExample/test/test_data_object/test_data_object.cpp
@@ -38,6 +38,73 @@ void basic_nested_data_object_test() {
                            expected_debug_string.c_str());
 }
 
+void basic_equivalence_test() {
+  auto data_object = create_object({
+      {"nullValue", create_null_value()},
+      {"fanSpeed", create_number_value(50)},
+      {"isOn", create_bool_value(true)},
+      {
+          "deviceInfo",
+          create_object({
+              {"deviceName", create_string_value("MyDevice92342")},
+              {"freeRAM", create_number_value(23239)},
+          }),
+      },
+      {
+          "humidityValues",
+          create_array({
+              create_number_value(34),
+              create_number_value(37),
+              create_number_value(40),
+              create_number_value(48),
+              create_number_value(50),
+          }),
+      },
+  });
+
+  TEST_ASSERT_TRUE(data_object->equals(data_object));
+
+  auto data_object1 = create_object({
+      {"foo", create_string_value("bar")},
+      {
+          "bar",
+          create_array({
+              create_number_value(1),
+              create_number_value(2),
+              create_number_value(3),
+              create_number_value(4),
+              create_number_value(5),
+          }),
+      },
+  });
+
+  auto data_object2 = create_object({
+      {"foo", create_string_value("bar")},
+      {
+          "bar",
+          create_array({
+              create_number_value(1),
+              create_number_value(2),
+              create_number_value(3),
+              create_number_value(4),
+              create_number_value(6),
+          }),
+      },
+  });
+
+  TEST_ASSERT_FALSE(data_object1->equals(data_object2));
+
+  auto data_object_with_int = create_object({
+      {"number", create_number_value(5)},
+  });
+
+  auto data_object_with_double = create_object({
+      {"number", create_number_value(5.0)},
+  });
+
+  TEST_ASSERT_TRUE(data_object_with_int->equals(data_object_with_double));
+}
+
 int main(int argc, char **argv) {
   UNITY_BEGIN();
 

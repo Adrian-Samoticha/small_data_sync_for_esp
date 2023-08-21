cacd895ee7db8229b0a3e91b9b4d58b3d390caa0
Adrian Samoticha <adrian@samoticha.de>
2023-08-21 17:03:27 +0200

    add `decode` and `encode` methods to `Codec`

--- src/Codec/Codec.h
+++ src/Codec/Codec.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <memory>
+#include <string>
+
+#include "DataObject/DataObject.h"
+#include "optional/include/tl/optional.hpp"
+
 struct Codec {
+  virtual tl::optional<std::shared_ptr<DataObject::GenericValue>> decode(
+      std::string encoded_data, std::string error_string) const = 0;
+
+  virtual std::string encode(
+      std::shared_ptr<DataObject::GenericValue> data) const = 0;
+
   virtual ~Codec() = default;
 };

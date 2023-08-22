864ccbbd7825ba43533c14c43fe1c523ba64469e
Adrian Samoticha <adrian@samoticha.de>
2023-08-22 19:29:17 +0200

    add virtual methods to `SyncedStruct`

--- src/SyncedStruct/SyncedStruct.h
+++ src/SyncedStruct/SyncedStruct.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <memory>
+#include <string>
+
+#include "Codec/Codec.h"
+#include "DataObject/DataObject.h"
 
 struct SyncedStruct {
+  virtual std::string get_name() const = 0;
+
+  virtual std::shared_ptr<DataObject::GenericValue> to_data_object() const = 0;
+
+  virtual bool apply_from_data_object(
+      std::shared_ptr<DataObject::GenericValue> data_object) = 0;
+
   virtual ~SyncedStruct() = default;
 };

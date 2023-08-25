b2793c8746328ae35dd916e3e3c7637d14cd8205
Adrian Samoticha <adrian@samoticha.de>
2023-08-25 19:15:27 +0200

    add `get_format` method to `Codec`

--- src/Codec/Codec.h
+++ src/Codec/Codec.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 
+#include "DataFormat/DataFormat.h"
 #include "DataObject/DataObject.h"
 #include "optional/include/tl/optional.hpp"
 
@@ -12,6 +13,8 @@ struct Codec {
 
   virtual std::string encode(
       std::shared_ptr<DataObject::GenericValue> data) const = 0;
+
+  virtual DataFormat get_format() const = 0;
 
   virtual ~Codec() = default;
 };

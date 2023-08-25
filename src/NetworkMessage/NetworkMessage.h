6fdf5d82201a116a04edde529e23b43acded8349
Adrian Samoticha <adrian@samoticha.de>
2023-08-25 19:17:06 +0200

    add `NetworkMessage`

---
+++ src/NetworkMessage/NetworkMessage.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+#include "DataObject/DataObject.h"
+
+struct NetworkMessage {
+  virtual std::shared_ptr<DataObject::GenericValue> to_data_object() const = 0;
+
+  virtual void on_send_succeeded() const {}
+
+  virtual void on_send_failed() const {}
+
+  virtual ~NetworkMessage() = default;
+};

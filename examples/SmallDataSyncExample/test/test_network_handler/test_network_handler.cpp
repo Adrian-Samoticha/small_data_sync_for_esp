cae1d1a79f39ed4e5a8cfaaaedfc1e3d2602d72c
Adrian Samoticha <adrian@samoticha.de>
2023-08-26 17:37:14 +0200

    add `basic_network_handler_test_with_packet_loss`

--- examples/SmallDataSyncExample/test/test_network_handler/test_network_handler.cpp
+++ examples/SmallDataSyncExample/test/test_network_handler/test_network_handler.cpp
@@ -1,5 +1,6 @@
 #include <unity.h>
 
+#include <cstdlib>
 #include <functional>
 #include <queue>
 
@@ -79,10 +80,66 @@ void basic_network_handler_test() {
   TEST_ASSERT_EQUAL(true, *has_received_message);
 }
 
+void basic_network_handler_test_with_packet_loss() {
+  std::srand(234823u);
+
+  auto network_simulator = utils::NetworkSimulator();
+  network_simulator.set_packet_loss_rate(0.9);
+
+  auto sender =
+      udp_interface::Endpoint(std::make_shared<utils::IPAddressImpl>(0), 0);
+  auto sender_network_handler = NetworkHandler();
+  auto sender_udp_interface = std::make_shared<utils::UdpInterfaceImpl>(
+      sender, sender_network_handler, network_simulator);
+  sender_network_handler.set_udp_interface(sender_udp_interface);
+
+  auto receiver =
+      udp_interface::Endpoint(std::make_shared<utils::IPAddressImpl>(1), 1);
+  auto receiver_network_handler = NetworkHandler();
+  auto receiver_udp_interface = std::make_shared<utils::UdpInterfaceImpl>(
+      receiver, receiver_network_handler, network_simulator);
+  receiver_network_handler.set_udp_interface(receiver_udp_interface);
+
+  network_simulator.register_endpoint(sender);
+  network_simulator.register_endpoint(receiver);
+
+  auto has_received_message = std::make_shared<bool>(false);
+
+  auto receiver_delegate = std::make_shared<NetworkHandlerDelegateImpl>(
+      [has_received_message](IncomingDecodedMessage message) {
+        TEST_PRINTF("Received.\n", 0);
+        if (message.data_object->is_null()) {
+          *has_received_message = true;
+        }
+      });
+  receiver_network_handler.set_delegate(receiver_delegate);
+
+  auto message = std::make_shared<NetworkMessageImpl>();
+  sender_network_handler.send_message(message, receiver, 100,
+                                      std::make_shared<JsonCodec>());
+
+  for (int i = 0; i < 200; ++i) {
+    if (*has_received_message) {
+      break;
+    }
+
+    for (int j = 0; j < 10; ++j) {
+      sender_network_handler.heartbeat();
+      receiver_network_handler.heartbeat();
+    }
+    sender_network_handler.on_100_ms_passed();
+    receiver_network_handler.on_100_ms_passed();
+  }
+
+  TEST_ASSERT_EQUAL_MESSAGE(true, *has_received_message,
+                            "basic_network_handler_test_with_packet_loss");
+}
+
 int main(int argc, char** argv) {
   UNITY_BEGIN();
 
   RUN_TEST(basic_network_handler_test);
+  RUN_TEST(basic_network_handler_test_with_packet_loss);
 
   return UNITY_END();
 }

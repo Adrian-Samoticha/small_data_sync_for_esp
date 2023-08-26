aa6547615b38eaa980598840e8f21fcbdb54998e
Adrian Samoticha <adrian@samoticha.de>
2023-08-27 00:09:41 +0200

    begin serial in `main.cpp`

--- examples/SmallDataSyncExample/src/main.cpp
+++ examples/SmallDataSyncExample/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 
-// #include "SmallDataSync.h"
+#include "SmallDataSync.h"
 
 void setup() {
+  Serial.begin(115200);
   Serial.printf("hello world (compiled at %s %s)\n", __DATE__, __TIME__);
 }
 

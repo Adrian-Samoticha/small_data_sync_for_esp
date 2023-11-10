056ca451bf5524a4083396f63525682a711549a4
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2023-11-10 10:40:15 +0100

    change documentation format for `EndpointInfo`

--- src/Synchronizer/EndpointInfo/EndpointInfo.h
+++ src/Synchronizer/EndpointInfo/EndpointInfo.h
@@ -3,7 +3,9 @@
 #include <string>
 
 namespace endpoint_info {
-/// @brief A struct containing an endpoint’s hostname and answer text.
+/**
+ * A struct containing an endpoint’s hostname and answer text.
+ */
 struct EndpointInfo {
  private:
   std::string hostname;
@@ -15,16 +17,19 @@ struct EndpointInfo {
   EndpointInfo(std::string hostname, std::string answer_text)
       : hostname(hostname), answer_text(answer_text) {}
 
-  /// @brief Returns the hostname.
-  /// @return The hostname string.
+  /**
+   * Returns the hostname.
+   */
   std::string get_hostname() const { return hostname; }
 
-  /// @brief Returns the answer text.
-  /// @return The answer text string.
+  /**
+   * Returns the answer text.
+   */
   std::string get_answer_text() const { return answer_text; }
 
-  /// @brief Converts this EndpointInfo to a string for debug purposes.
-  /// @return A string containing the hostname and answer text.
+  /**
+   * Converts this `EndpointInfo` to a string for debug purposes.
+   */
   std::string to_string() const {
     return "Hostname: " + hostname + ", Answer: " + answer_text;
   }

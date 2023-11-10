c4d89f1395b1f5e14aa64c177fe0ecc520ddb6ed
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2023-11-10 10:29:12 +0100

    document `EndpointInfo`

--- src/Synchronizer/EndpointInfo/EndpointInfo.h
+++ src/Synchronizer/EndpointInfo/EndpointInfo.h
@@ -3,6 +3,7 @@
 #include <string>
 
 namespace endpoint_info {
+/// @brief A struct containing an endpoint’s hostname and answer text.
 struct EndpointInfo {
  private:
   std::string hostname;
@@ -14,10 +15,16 @@ struct EndpointInfo {
   EndpointInfo(std::string hostname, std::string answer_text)
       : hostname(hostname), answer_text(answer_text) {}
 
+  /// @brief Returns the hostname.
+  /// @return The hostname string.
   std::string get_hostname() const { return hostname; }
 
+  /// @brief Returns the answer text.
+  /// @return The answer text string.
   std::string get_answer_text() const { return answer_text; }
 
+  /// @brief Converts this EndpointInfo to a string for debug purposes.
+  /// @return A string containing the hostname and answer text.
   std::string to_string() const {
     return "Hostname: " + hostname + ", Answer: " + answer_text;
   }
